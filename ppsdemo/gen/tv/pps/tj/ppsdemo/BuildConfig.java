2839d1bcb96f05dcfb2ff2daedf3b90975fce0ae
tujun <jasontujun@163.com>
2013-07-16 18:45:33 +0800

    v0.1 setup project

---
+++ ppsdemo/gen/tv/pps/tj/ppsdemo/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package tv.pps.tj.ppsdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
